fec88e128a818fb1473ab400869ba8f96efaef20
shenwl <shenweilun96@gmail.com>
2021-07-06 22:03:37 +0800

    Scharr算子

---
+++ filter/scharr.py
@@ -0,0 +1,24 @@
+import cv2
+import numpy as np
+
+img = cv2.imread("../assets/cat.jpg")
+src = cv2.resize(img, None, fx=0.2, fy=0.2)
+
+# 索贝尔算子y方向边缘
+d1 = cv2.Scharr(src, cv2.CV_64F, 1, 0)
+# 索贝尔算子x方向边缘
+d2 = cv2.Scharr(src, cv2.CV_64F, 0, 1)
+
+dst = d1 + d2
+
+cv2.imshow('src', src)
+cv2.imshow('y edge', d1)
+cv2.imshow('x edge', d2)
+cv2.imshow('add dx, dy', dst)
+
+while True:
+  key = cv2.waitKey(1) & 0xff
+  if key == ord('q'):
+    break
+
+cv2.destroyAllWindows()
